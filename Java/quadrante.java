8713bab6a77b6067cab023005bf685cd38efbe26
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-08-05 01:28:39 -0300

    Add files via upload

---
+++ Java/quadrante.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class quadrante {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+
+
+        sc.close();
+
+    }
+}
