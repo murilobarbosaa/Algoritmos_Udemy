d692d6ba185651bb7ce125d3291b3d6bbabe432a
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-08-21 13:44:52 -0300

    Add files via upload

---
+++ C/soma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int x, y;
+
+int main() {
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+    printf("Digite o valor de y: ");
+    scanf("%d", &y);
+
+    printf("SOMA = %d", x + y);
+}
