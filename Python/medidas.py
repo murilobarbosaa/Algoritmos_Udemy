f97ee936eac6c09a2c7746d5b776d020d4ecc2c0
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-08-21 00:54:45 -0300

    Add files via upload

---
+++ Python/medidas.py
@@ -0,0 +1,11 @@
+a = float(input("Digite a medida A: "))
+b = float(input("Digite a medida B: "))
+c = float(input("Digite a medida C: "))
+
+quadrado = a * a
+triangulo = a * b / 2
+trapezio = (a + b) * c / 2
+
+print(f"AREA DO QUADRADO = {quadrado:.2f}")
+print(f"AREA DO TRIÂNGULO = {triangulo:.2f}")
+print(f"AREA DO TRAPÉZIO = {trapezio:.2f}")
